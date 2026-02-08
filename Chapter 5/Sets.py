bbf2a113c627c7b3aa79b25a44f0a72c671157d7
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-08 23:39:00 +0600

    tenth commit

---
+++ Chapter 5/Sets.py
@@ -0,0 +1,8 @@
+'''Set is also a data structure in python. It is an unordered
+collection of items. Every element is unique (no duplicates) 
+and must be immutable (cannot be changed). However, a set 
+itself is mutable. We can add or remove items from it. 
+The set class in python is based on the mathematical concept 
+of a set. It is a collection of distinct objects,
+considered as an object in its own right.'''
+
