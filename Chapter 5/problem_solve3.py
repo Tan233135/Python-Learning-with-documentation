4c8ec01acbb284436ad1c7c9e8e0198583c4e38f
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 01:20:48 +0600

    new commit

---
+++ Chapter 5/problem_solve3.py
@@ -0,0 +1,8 @@
+'''Can we have a set with 18(int) and '18'(str) as a value in it?'''
+
+'''Yes, we can have a set with both 18(int) and '18'(str) as values in it.
+This is because sets in Python can contain elements of different data types,
+ and both 18(int) and '18'(str) are considered distinct elements.'''
+
+s = {18, '18'} #This is a set with both 18(int) and '18'(str) as values in it
+print(s) #This will print the set with both 18(int) and '18'(str) as values in it
