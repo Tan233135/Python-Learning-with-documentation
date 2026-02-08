067fc24f1ef55ec5dd1f54d57b22c1a121253fce
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 01:12:00 +0600

    new commit

---
+++ Chapter 5/set operations.py
@@ -0,0 +1,19 @@
+s1= {1,2,3}
+s2={3,4,5}
+
+'''The union() method can be used to get the union of two sets.'''
+union_set = s1.union(s2) #This will return a new set that is the union of s1 and s2
+print(union_set) #This will print the union of s1 and s2
+
+'''The intersection() method can be used to get the intersection of two sets.'''
+intersection_set = s1.intersection(s2) #This will return a new set that is the intersection of s1 and s2
+print(intersection_set) #This will print the intersection of s1 and s2
+
+'''The difference() method can be used to get the difference of two sets.'''
+difference_set = s1.difference(s2) #This will return a new set that is the difference of s1 and s2
+print(difference_set) #This will print the difference of s1 and s2
+
+'''The symmetric_difference() method can be used to get the symmetric difference
+of two sets.'''
+symmetric_difference_set = s1.symmetric_difference(s2) #This will return a new set that is the symmetric difference of s1 and s2
+print(symmetric_difference_set) #This will print the symmetric difference of s1 and s2
