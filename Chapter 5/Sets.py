1e49cc70c853b61b801da49d212fad20d83acd99
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-09 00:24:22 +0600

    thirtinth commit

--- Chapter 5/Sets.py
+++ Chapter 5/Sets.py
@@ -6,3 +6,10 @@ The set class in python is based on the mathematical concept
 of a set. It is a collection of distinct objects,
 considered as an object in its own right.'''
 
+s = set() #This is an empty set
+
+s = {1,2,3,4,5} #This is a set with some elements
+
+print(s,type(s)) #This will print the set and its type
+
+s = {1,2,3, "Tanvir", (1,2)} #This is a set with different types of elements
