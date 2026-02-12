a70eeb46af5adc96f07847285415bb606ebf01b0
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-12 21:47:15 +0600

    new

--- Chapter 7/For_loop.py
+++ Chapter 7/For_loop.py
@@ -2,10 +2,7 @@
 there are two types of loops: for loops and while loops. 
 
 For loops are used to iterate over a sequence (like a list, tuple, or
-string or range) and execute a block of code for each item in the sequence.
-
-while loops are used to execute a block of code as long as a certain condition is true.
-'''
+string or range) and execute a block of code for each item in the sequence.'''
 
 # For loop example
 fruits = ['apple', 'banana', 'cherry']
@@ -23,9 +20,3 @@ for num in range(1,11):
         print(f"{num} is even")
     else:
         print(f"{num} is odd")
-
-# While loop example
-count = 0
-while (count < 5):
-    print(count)
-    count += 1
---
+++ Chapter 7/While_loop.py
@@ -0,0 +1,8 @@
+'''while loops are used to execute a block of code as long as a certain condition is true.
+'''
+# While loop example
+count = 0
+while (count < 5):
+    print(count)
+    count += 1
+
