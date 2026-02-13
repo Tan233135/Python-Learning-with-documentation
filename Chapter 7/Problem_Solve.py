49b4399af4ed80209f51fa12101fe8c3ecfd81d3
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-13 19:24:08 +0600

    new

---
+++ Chapter 7/Problem_Solve.py
@@ -0,0 +1,7 @@
+'''Write a program to print multiplication table of a given number using for loop.'''
+
+num = int(input("Enter the number you want the multiplication of:\t"))
+for i in range(1,11):
+    print(f"{num} x {i} =  ",num*i)
+
+''''''
