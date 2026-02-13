407a8cdbbdde4c23e1d471fc04a4b343b40e23df
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-13 19:39:17 +0600

    new

--- Chapter 7/Problem_Solve.py
+++ Chapter 7/Problem_Solve.py
@@ -4,4 +4,14 @@ num = int(input("Enter the number you want the multiplication of:\t"))
 for i in range(1,11):
     print(f"{num} x {i} =  ",num*i)
 
-''''''
+'''Write a program to greet all the person names stored in a list 'l' and which 
+starts with S.'''
+
+l = []
+n=int(input("Enter the amount of names you want to store:\t"))
+for i in range(n):
+    l.append(input(f"Enter the name of {i+1}:\t"))
+
+for i in range(n):
+    if(l[i][0]=="S"):
+        print(f"Good Morning, {l[i]}")
