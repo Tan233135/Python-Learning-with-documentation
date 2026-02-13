79226890b631cab564dd893ece866e4983ae4386
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-13 19:15:18 +0600

    new

---
+++ Chapter 7/Break__Continue.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i==34):
+        break  #Exit the Loop right Now
+    print(i)
+
+for i in range(100):
+    if(i==34):
+        continue #Skip this iteration
+    print(i)
