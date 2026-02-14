469d254060539f4763b0b0badef04bf30864b60a
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-14 23:54:30 +0600

    new

--- Chapter 7/Pattern printing.py
+++ Chapter 7/Pattern printing.py
@@ -4,8 +4,11 @@ for i in range(1,n+1):
     print("*"*(2*i-1),end="")
     print("\n")
 
-print("\n\n\n")
+print("\n")
 
 for i in range(1,n+1):
     print("*"*i,end="")
     print("")
+
+print("\n")
+
