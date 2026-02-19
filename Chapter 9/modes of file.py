818483381d80a2388623c315db61e8712fedb6b9
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-20 02:29:48 +0600

    new

--- Chapter 9/modes of file.py
+++ Chapter 9/modes of file.py
@@ -10,3 +10,9 @@ st = "Hey tanvir you are learning python"
 f = open("file.txt","a")
 f.write(st)
 f.close() #Everytime we run this code due to the append mode a string will be added in the file
+
+#The same can be written using with statement like this:
+with open("file.txt") as f:
+    print(f.read())
+
+#By using this format we don't need to close the file
