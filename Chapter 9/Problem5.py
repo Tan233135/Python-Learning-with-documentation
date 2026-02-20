2e05505331c6f54edf106d2560cd817ef5a06c64
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-21 01:06:19 +0600

    new

--- Chapter 9/Problem5.py
+++ Chapter 9/Problem5.py
@@ -3,7 +3,8 @@ words = ["Tanvir","Sami","Mishkat","Shuvo"]
 with open("E:\python learning\Chapter 9\lists.txt","r") as f:
     content = f.read()
 
+for word in words:
+    content = content.replace(word,"#####")
 
 with open("E:\python learning\Chapter 9\lists.txt","w") as f:
-    for i in range(len(words)):
-        f.write(f"{content.replace(f"{words[i]}","#####")}")
+    f.write(content)
