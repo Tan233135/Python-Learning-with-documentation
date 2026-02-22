0f77fa04c5d948f279f2ca1bf4b772179e613bc0
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-22 23:20:11 +0600

    new

--- Chapter 10/class.py
+++ Chapter 10/class.py
@@ -1,7 +1,11 @@
 class Employee:
-    name = "Tanvir"
     language = "py"
     salary = 1200000
 
 Tanvir = Employee()
+Tanvir.name = "Tanvir"
 print(Tanvir.name, Tanvir.language, Tanvir.salary)
+
+'''Here name is object attribute
+Salary and language are class attributes as
+they directly belong to the class'''
