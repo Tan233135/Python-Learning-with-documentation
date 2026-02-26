d4eb19e96b08c823ff3c6e4381f4a0885726f326
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-27 00:27:54 +0600

    new

---
+++ Project 2/Perfect Guess.py
@@ -0,0 +1,14 @@
+import random
+n = random.randint(1,100)
+a = -1
+guesses = 0
+while(a!=n):
+    guesses += 1
+    a = int(input("Guess the Number: "))
+    if(a>n):
+        print("Lower number please")
+    else:
+        print("Higher number please")
+
+print(f"You have guesses the number correctly in {guesses} guesses")
+
