ac61fd441d9ad2ebc8e31ea62957ac8db64a81c8
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-27 00:14:20 +0600

    new

--- Chapter 11/inheritance.py
+++ Chapter 11/inheritance.py
@@ -3,11 +3,16 @@ class Employee:
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
 
-class Programmer:
+'''class Programmer:
     company = "ITC Infotech"
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
     
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+'''
+class Programmer(Employee):
+    company = "ITC Infotech"
     def showLanguage(self):
         print(f"The name is {self.name} and he is good with {self.language} language")
 
